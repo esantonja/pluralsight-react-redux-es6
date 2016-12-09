74c8f3868a15cf4525d3d833871d580daf85a86e
Leanne Jagger <leanne.jagger@skybettingandgaming.com>
2016-12-09 09:20:27 +0000

    Replace magic strings with action type constants

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,7 @@
 // This is an action creator
+
+import * as types from './actionTypes';
+
 export function createCourse(course) {
-  return {type: 'CREATE_COURSE', course: course};
+  return {type: types.CREATE_COURSE, course: course};
 }
