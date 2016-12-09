60b03d7c0c2d78efe11703abaa72dcb060d0bd8d
Leanne Jagger <leanne.jagger@skybettingandgaming.com>
2016-12-09 10:26:34 +0000

    Add Thunk middleware, create loadCourse thunk and add loadCourseSuccess action/type constant.

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,7 +1,18 @@
 // This is an action creator
 
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return {type: types.CREATE_COURSE, course: course};
+export function loadCoursesSuccess(courses) {
+  return {type: types.LOAD_COURSES_SUCCESS, courses: courses};
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
 }
