5af485cba642b5a818f280c63935232c04eacbc8
Leanne Jagger <leanne.jagger@skybettingandgaming.com>
2016-12-09 11:13:13 +0000

    Introduce a container component for managing a course and add routing for the new page

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import App from './components/App.react';
 import HomePage from './components/home/HomePage.react';
 import AboutPage from './components/about/AboutPage.react';
 import CoursesPage from './components/course/CoursesPage.react';
+import ManageCoursePage from './components/course/ManageCoursePage.react'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="courses" component={CoursesPage}/>
+    <Route path="course" component={ManageCoursePage}/>
+    <Route path="course/:id" component={ManageCoursePage}/>
     <Route path="about" component={AboutPage}/>
   </Route>
 );
